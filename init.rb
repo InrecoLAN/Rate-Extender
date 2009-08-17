52d64553415cb44d12e35aafbfa54df49958e219
Ivan Astafyev <ivan.astafyev@inrecolan.com>
2009-08-17 16:59:43 +0400

    Adds requirements

--- init.rb
+++ init.rb
@@ -5,4 +5,6 @@ Redmine::Plugin.register :redmine_rate_extender do
   author 'Ivan Astafyev'
   description 'A plugin for extending Eric Davis\' Rate Plugin'
   version '0.0.1'
+  requires_redmine :version_or_higher => '0.8.0'
+  Redmine::Plugin.find(:redmine_rate)
 end
